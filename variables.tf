85243c871b1fb75bf4cda1a8fbd1c4e031a925a1
Mohamed Belgaied Hassine <belgaied2@hotmail.com>
2020-09-10 13:51:25 +0200

    Variabilizing all fixed values

--- variables.tf
+++ variables.tf
@@ -32,4 +32,61 @@ variable "vm_count" {
   default = 3
 }
 
+variable "vsphere_dc" {
+  description = "vSphere Datacenter to use to create VMs"
+}
+
+variable "vsphere_ds" {
+  description = "vSphere Datastore to use to create VMs"
+}
+
+variable "vsphere_rp" {
+  description = "vSphere Resource Pool to attribute the VMs to"
+}
+
+variable "vsphere_net" {
+  description = "vSphere Network to attribute the VMs to"
+}
+
+variable "vm_cpus" {
+  description = "Number of CPUs to give to VM"
+  default     = 2
+}
+
+variable "vm_mem" {
+  description = "Memory size in MB for VM"
+  default     = 8192
+}
+
+variable "vm_disk_size" {
+  description = "Size in GB of the main VM's Disk"
+  default     = 20
+}
+
+variable "vsphere_guest_id" {
+  description = "Type of OS for Guest"
+  default     = "rhel7_64Guest"
+}
+
+variable "ssh_user" {
+  description = "SSH Username to connect to VM with"
+}
+
+variable "vm_name_prefix" {
+  description = "Prefix for the VM name in vSphere"
+  default     = "rancher-ha"
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
 
--- vms.tf
+++ vms.tf
@@ -1,19 +1,19 @@
 data "vsphere_datacenter" "dc" {
-  name = "RNCH-HE-FMT"
+  name = var.vsphere_dc
 }
 
 data "vsphere_datastore" "datastore" {
-  name          = "ranch01-silo01-vm01"
+  name          = var.vsphere_ds
   datacenter_id = data.vsphere_datacenter.dc.id
 }
 
 data "vsphere_resource_pool" "pool" {
-  name          = "mbh"
+  name          = var.vsphere_rp
   datacenter_id = data.vsphere_datacenter.dc.id
 }
 
 data "vsphere_network" "network" {
-  name          = "Private Range 172.16.128.1-21"
+  name          = var.vsphere_net
   datacenter_id = data.vsphere_datacenter.dc.id
 }
 
@@ -24,13 +24,13 @@ data "vsphere_virtual_machine" "template" {
 
 resource "vsphere_virtual_machine" "vm" {
   count = var.vm_count
-  name             = "rancher-ha-${count.index}"
+  name             = "${var.vm_name_prefix}-${count.index}"
   resource_pool_id = data.vsphere_resource_pool.pool.id
   datastore_id     = data.vsphere_datastore.datastore.id
 
-  num_cpus = 2
-  memory   = 8192
-  guest_id = "rhel7_64Guest"
+  num_cpus = var.vm_cpus
+  memory   = var.vm_mem
+  guest_id = var.vsphere_guest_id
 
   network_interface {
     network_id = data.vsphere_network.network.id
@@ -38,7 +38,7 @@ resource "vsphere_virtual_machine" "vm" {
 
   disk {
     label = "disk0"
-    size  = 20
+    size  = var.vm_disk_size
     thin_provisioned = false
   }
 
@@ -49,7 +49,7 @@ resource "vsphere_virtual_machine" "vm" {
   provisioner "remote-exec" {
     connection {
       type     = "ssh"
-      user     = "rancher"
+      user     = var.ssh_user
       private_key = file(var.private_key)
       host     = self.default_ip_address
     }
